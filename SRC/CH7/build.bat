e2251adea998ac3638b7fef4f56cbac5a6c8d009
bluesentinelsec <bluesentinel@protonmail.com>
2023-09-04 19:01:15 -0400

    added ch7

---
+++ SRC/CH7/build.bat
@@ -0,0 +1,12 @@
+BCC.EXE -mm -B -c -v -j1 black3.c
+BCC.EXE -mm -B -c -v -j1 black4.c
+BCC.EXE -mm -B -c -v -j1 black5.c
+BCC.EXE -mm -B -c -v -j1 black6.c
+BCC.EXE -mm -B -v -j1 critters.c black3.obj black4.obj black5.obj black6.obj
+BCC.EXE -mm -B -v -j1 floater.c black3.obj black4.obj black5.obj black6.obj
+BCC.EXE -mm -B -v -j1 jumper.c black3.obj black4.obj black5.obj black6.obj
+BCC.EXE -mm -B -v -j1 lockon.c black3.obj black4.obj black5.obj black6.obj
+BCC.EXE -mm -B -v -j1 lostnspc.c black3.obj black4.obj black5.obj black6.obj
+
+REM critters.c  floater.c  jumper.c  lockon.c  lostnspc.c
+
