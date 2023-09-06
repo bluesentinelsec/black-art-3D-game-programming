4e6c9bafa5ec42094e9b48bed3c35e86f05d2888
bluesentinelsec <bluesentinel@protonmail.com>
2023-09-06 06:11:14 -0400

    added chapter's 9 and 11

---
+++ SRC/CH9/build.bat
@@ -0,0 +1,15 @@
+BCC.EXE -mm -B -c -v -j1 black3.c
+BCC.EXE -mm -B -c -v -j1 black4.c
+BCC.EXE -mm -B -c -v -j1 black5.c
+BCC.EXE -mm -B -c -v -j1 black6.c
+BCC.EXE -mm -B -c -v -j1 black8.c
+BCC.EXE -mm -B -c -v -j1 black9.c
+
+BCC.EXE -mm -B -v -j1 blazem0.c blazer.c blazem1.c blazem2.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj
+REM BCC.EXE -mm -B -v -j1 blazem1.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj
+REM BCC.EXE -mm -B -v -j1 blazem2.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj
+REM BCC.EXE -mm -B -v -j1 blazer.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj
+REM BCC.EXE -mm -B -v -j1 term1.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj
+REM BCC.EXE -mm -B -v -j1 term2.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj
+
+
