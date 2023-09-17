6d39591c909720ad49a60fc80e26e356676111f1
bluesentinelsec <bluesentinel@protonmail.com>
2023-09-17 11:09:33 -0400

    added CH14

---
+++ SRC/CH14/build.bat
@@ -0,0 +1,10 @@
+BCC.EXE -mm -B -c -v -j1 black3.c
+BCC.EXE -mm -B -c -v -j1 black4.c
+BCC.EXE -mm -B -c -v -j1 black5.c
+BCC.EXE -mm -B -c -v -j1 black6.c
+BCC.EXE -mm -B -c -v -j1 black8.c
+BCC.EXE -mm -B -c -v -j1 black9.c
+BCC.EXE -mm -B -c -v -j1 black11.c
+
+BCC.EXE -mm -B -v -j1 objects.c black3.obj black4.obj black5.obj black6.obj black8.obj black9.obj black11.obj
+
